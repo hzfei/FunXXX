54d5ade554ad42501716e7575dab9221fb016077
胡真 <huzhen@viomi.com.cn>
2023-08-23 10:46:19 +0800

    init

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ with open("README.md", "r",encoding='utf8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="my_bert",                                     # 包的分发名称，使用字母、数字、_、-
+    name="funxxx",                                     # 包的分发名称，使用字母、数字、_、-
     version="0.0.1",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
     author="hzfei",                                       # 作者名字
     author_email="1027551108@qq.com",                      # 作者邮箱
-    description="just a bert tool",                            # 包的简介描述
+    description="just a xxx tool",                            # 包的简介描述
     long_description=long_description,                      # 包的详细介绍(一般通过加载README.md)
     long_description_content_type="text/markdown",          # 和上条命令配合使用，声明加载的是markdown文件
     url="https://github.com/users/hzfei",                              # 项目开源地址，我这里写的是同性交友官网，大家可以写自己真实的开源网址
